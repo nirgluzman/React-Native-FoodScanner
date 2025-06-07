ebde7226a9b2f293af554a39da3161bb6bf964a0
Nir Gluzman <nir.gluzman@gmail.com>
2025-06-07 17:43:17 +0200

    feat: add Nunito font family and definitions

---
+++ constants/fonts.ts
@@ -0,0 +1,21 @@
+// Nunito font family
+// This file exports the Nunito font family styles for use in the application.
+
+export default {
+  nblack: ['Nunito-Black', 'sans-serif'],
+  nblackitalic: ['Nunito-BlackItalic', 'sans-serif'],
+  nbold: ['Nunito-Bold', 'sans-serif'],
+  nbolditalic: ['Nunito-BoldItalic', 'sans-serif'],
+  nextrabold: ['Nunito-ExtraBold', 'sans-serif'],
+  nextrabolditalic: ['Nunito-ExtraBoldItalic', 'sans-serif'],
+  nextralight: ['Nunito-ExtraLight', 'sans-serif'],
+  nextralightitalic: ['Nunito-ExtraLightItalic', 'sans-serif'],
+  nitalic: ['Nunito-Italic', 'sans-serif'],
+  nlight: ['Nunito-Light', 'sans-serif'],
+  nlightitalic: ['Nunito-LightItalic', 'sans-serif'],
+  nmedium: ['Nunito-Medium', 'sans-serif'],
+  nmediumitalic: ['Nunito-MediumItalic', 'sans-serif'],
+  nregular: ['Nunito-Regular', 'sans-serif'],
+  nsemibold: ['Nunito-SemiBold', 'sans-serif'],
+  nsemibolditalic: ['Nunito-SemiBoldItalic', 'sans-serif'],
+};
